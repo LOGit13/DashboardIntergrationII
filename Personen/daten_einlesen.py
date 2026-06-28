c317fb1bdba7744437da9e01783bb5471a1d75aa
Noah Reinermann <rn6808@mci4me.at>
2026-06-28 13:24:49 +0200

    daten_einlesen und klasse_personen fertig

--- Personen/daten_einlesen.py
+++ Personen/daten_einlesen.py
@@ -1,3 +1,62 @@
 import json
 import pandas as pd
+import os
+
+def personen_einlesen(pfad):
+    """
+    JSON Datei wird eingelesen und gibt alle gespeicherten Personenifos zurück
+    Die Datei wird geöffnet, ausgelesen und als Python-Datenstruktur zurückgegeben.
+    """
+    with open(pfad, "r", encoding="utf-8") as ordner:
+        daten = json.load(ordner)
+    return daten
+
+def extrahiere_name(personen):
+    """
+    sortierte Liste der Namen wird erstellt. Personen lassen sich später so in Dropdowns anzeigen.
+    """
+    namen = []
+    for person in personen:
+        name = "{}, {}".format(person["lastname"], person["firstname"])
+        namen.append(name)
+    return sorted(namen)
+
+def person_finden_mit_name(name, daten):
+    """
+    in der Personenliste wird nach einer Person mit 'Nachname, Vorname' gesucht.
+    es wird ein Personen-Dictionary zurückgegeben, wenn die Person gefunden wurde, ansonsten ein leeres Dictionary.
+    """
+    if name is None or name == "None":
+        return {}
+    
+    teile = name.split(", ")
+    if len(teile) != 2:
+        return {}   
+    
+    nachname, vorname = teile
+
+    return next((person for person in daten if person["lastname"] == nachname and person["firstname"] == vorname), {})
+
+def person_finden_mit_id(id_wert, daten):
+    """
+    Person wird mithilfe der ID in Personliste gesucht. 
+    Personen-Dictionary wird zurückgegeben, wenn die Person gefunden wurde, ansonsten ein leeres Dictionary.
+    """
+    if id_wert is None:
+        return {}
+    for person in daten:
+        if str(person.get("id")) == str(id_wert):
+            return person
+    return {}
+
+def txt_zu_df(pfad):
+    """
+    es wird geprüft, ob die Datei existiert, und sie wird dann als Tabelle (DataFrame) eingelesen.
+    Wenn die Datei fehlt, kommt es zu einer Meldung und ein leerer DataFrame wird zurückgegeben
+    """ 
+    if os.path.exists(pfad):
+        return pd.read_csv(pfad, encoding="utf-8")
+    else:
+        print("Datei wurde nicht gefunden:", pfad)
+        return pd.DataFrame()
 
--- Personen/klasse_person.py
+++ Personen/klasse_person.py
@@ -1,5 +1,39 @@
-import json
 from datetime import datetime
 
 class Person:
+    def __init__(self, person_dict):
+        """ 
+        Personenobjekt wird angelegt und alle Daten werden übernommen.
+        """
+        self.vorname = person_dict.get("firstname", "")
+        self.nachname = person_dict.get("lastname", "")
+        self.bildpfad = person_dict.get("picture_path", "")
+        self.id = person_dict.get("id", None)
+
+        geboren = str(person_dict.get("date_of_birth", ""))
+        if len(geboren) == 4 and geboren.isdigit():
+            self.geburtsdatum = f"{geboren}-01-01"
+        else:
+            self.geburtsdatum = geboren
+
+    def berechne_alter(self):
+        """
+        Alter der Person wird berechnet, indem das Geburtsjahr von dem aktuellen Jahr subtrahiert wird.
+        """
+        geburt= str(self.geburtsdatum)
+        if "-" in geburt:
+            jahr = int(geburt.split("-")[0])
+        else:
+            jahr = int(geburt)
+        return datetime.now().year - jahr
+    
+    def berechne_max_puls(self):
+        """
+        Maximalpuls wird anhand dem Alter der Person berechnet.
+        """
+        alter = self.berechne_alter()
+        if alter <= 0:
+            return None
+        max_puls = 220 - alter
+        return max_puls
     
